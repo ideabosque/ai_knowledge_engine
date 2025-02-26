6b1fda60365a1ae45bbac4c7a4aed0ef517dfad9
Barry Liu <barry.y.liu@hotmail.com>
2025-02-26 22:33:37 +0800

    [U] Optizime code

--- ai_knowledge_engine/tests/test_ai_knowledge_engine.py
+++ ai_knowledge_engine/tests/test_ai_knowledge_engine.py
@@ -676,7 +676,7 @@ class AIKnowledgeEngineTest(unittest.TestCase):
         response = self.ai_knowledge_engine.ai_knowledge_graphql(**payload)
         logger.info(response)
 
-    @unittest.skip("demonstrating skipping")
+    # @unittest.skip("demonstrating skipping")
     def test_graphql_knowledge_rag(self):
         query = Utility.generate_graphql_operation("knowledgeRag", "Query", self.schema)
         logger.info(f"Query: {query}")
@@ -716,7 +716,7 @@ class AIKnowledgeEngineTest(unittest.TestCase):
         response = self.ai_knowledge_engine.ai_knowledge_graphql(**payload)
         logger.info(response)
 
-    # @unittest.skip("demonstrating skipping")
+    @unittest.skip("demonstrating skipping")
     def test_load_document(self):
         try:
             payload = {
